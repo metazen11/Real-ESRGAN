642f8a208d13e4ba4ea5a2c4a5588bf040a03177
MZ <83803364+metazen11@users.noreply.github.com>
2023-04-21 17:37:22 -0700

    batch file to run via single parameter on windows

---
+++ up.bat
@@ -0,0 +1,24 @@
+﻿@echo off
+
+set "InputPath=%~1"
+set "OutputPath="
+
+if exist "%InputPath%\" (
+  set "OutputPath=up_%InputPath%"
+) else if exist "%InputPath%" (
+  for %%F in ("%InputPath%") do (
+    set "OutputPath=%%~dpnF_up%%~xF"
+  )
+)
+
+if not defined OutputPath (
+  echo Invalid input path: "%InputPath%"
+  exit /b 1
+)
+
+echo Input path: "%InputPath%"
+echo Output path: "%OutputPath%"
+
+c:\up\realesrgan-ncnn-vulkan.exe -i "%InputPath%" -o "%OutputPath%" -n realesrgan-x4plus -s 4 -f png
+
+echo Upscaling complete.
